cccee62212916d1095e12a0a53aebb07492e3b47
alex <alexandersegerev@gmail.com>
2024-12-02 15:15:57 -0500

    Added the necessary components

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,4 @@ let articleTitle = document.querySelector('article h3');
 articleTitle.style.fontSize = '35px';
 
 let header = document.getElementById('header');
-header.style.color = 'yellow';
+header.style.color = 'blue';
